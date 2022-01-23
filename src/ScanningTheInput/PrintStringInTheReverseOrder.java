f70f62bf5d25d6767c27df70902e5399ce798e1a
anmol <anmolsoin1@gmail.com>
2022-01-24 01:45:34 +0530

    ProjectWork

---
+++ src/ScanningTheInput/PrintStringInTheReverseOrder.java
@@ -0,0 +1,16 @@
+package ScanningTheInput;
+
+import java.util.Scanner;
+
+public class PrintStringInTheReverseOrder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        String str3 = sc.next();
+
+        System.out.println(str3);
+        System.out.println(str2);
+        System.out.println(str1);
+    }
+}
