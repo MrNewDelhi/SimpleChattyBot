6d49ab99c87af7a611ee90e7d1792cee7edb3ab1
anmol <anmolsoin1@gmail.com>
2022-01-24 01:41:53 +0530

    ProjectWork

---
+++ src/ScanningTheInput/PrintingEachWordInANewLine.java
@@ -0,0 +1,20 @@
+package ScanningTheInput;
+
+import java.util.Scanner;
+
+public class PrintingEachWordInANewLine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        String str3 = sc.next();
+        String str4 = sc.next();
+        String str5 = sc.next();
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str4);
+        System.out.println(str5);
+    }
+}
