9f555053075d7f4a209c30ecfb808f633b1602ef
anmol <anmolsoin1@gmail.com>
2022-01-27 20:00:24 +0530

    ProjectWork

---
+++ src/IntegerTypesAndOperations/ReadingIntegerNumbers.java
@@ -0,0 +1,19 @@
+package IntegerTypesAndOperations;
+
+import java.util.Scanner;
+
+public class ReadingIntegerNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        int num4 = sc.nextInt();
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+    }
+}
