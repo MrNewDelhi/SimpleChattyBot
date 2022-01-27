0c0de80a9d31d7f7d13d83aafc324f4e74bce771
anmol <anmolsoin1@gmail.com>
2022-01-27 18:44:20 +0530

    ProjectWork

--- src/IntegerTypesAndOperations/Reversing.java
+++ src/IntegerTypesAndOperations/Reversing.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class Reversing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int original = sc.nextInt();
 
+        int digit = original % 10;
+        int reverse = digit;
+        original = original / 10;
+
+        digit = original % 10;
+        reverse = reverse * 10 + digit;
+        original = original / 10;
+
+        digit = original % 10;
+        reverse = reverse * 10 + digit;
+        original = original / 10;
+
+        System.out.println(reverse);
     }
 }
